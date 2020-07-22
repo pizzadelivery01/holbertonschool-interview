5d057bad6d728a75dd471115f23c8382f0e2585b
jeremy bedolla <jjbedolla@gmail.com>
2020-07-21 22:22:34 -0400

    empty box

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,7 +10,7 @@ def canUnlockAll(boxes):
     by calling function to get collectable keys
     then compare to amount of boxes
     """
-    if boxes != []:
+    if boxes:
         keys = [0]
         needed = [i[0] for i in enumerate(boxes)]
 
@@ -22,7 +22,7 @@ def canUnlockAll(boxes):
             return False
 
     else:
-        return False
+        return True
 
 
 def getKeys(boxes, keys, key=0):
