97eebac12dc3922546684b4bb604f48914f813a5
jeremy bedolla <jjbedolla@gmail.com>
2020-08-17 17:12:56 -0400

    adding files

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,112 @@
+#include "sandpiles.h"
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    add_grids(grid1, grid2);
+    while(!stable_check(grid1))
+    {
+        print_grid(grid1);
+        toppling(grid1);
+
+    }
+}
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+static void add_grids(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+}
+static int stable_check(int grid[3][3])
+{
+	int i, j;
+    int empty[3][3];
+    for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			empty[i][j] = 0;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (0);
+	return (1);
+}
+static void toppling(int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] = grid1[i][j] - 4;
+				if ((i - 1 >= 0) && (i - 1 < 3))
+                {
+                    if ( grid1[i - 1][j] < grid1[i][j])
+                    {
+                        grid1[i - 1][j] += 1;
+                    }
+                    else
+                    {
+                        grid1[i][j] += 1;
+                    }
+                    
+                }
+                if ((j - 1 >= 0) && (j - 1 < 3))
+                {
+                    if ( grid1[i][j - 1] < grid1[i][j])
+                    {
+                        grid1[i][j - 1] += 1;
+                    }
+                    else
+                    {
+                        grid1[i][j] += 1;
+                    }
+                    
+                }
+				if ((i + 1 >= 0) && (i + 1 < 3))
+                {
+                    if ( grid1[i + 1][j] < grid1[i][j])
+                    {
+                        grid1[i + 1][j] += 1;
+                    }
+                    else
+                    {
+                        grid1[i][j] += 1;
+                    }
+                    
+                }
+				if ((j + 1 >= 0) && (j + 1 < 3))
+                {
+                    if ( grid1[i][j + 1] < grid1[i][j])
+                    {
+                        grid1[i][j + 1] += 1;
+                    }
+                    else
+                    {
+                        grid1[i][j] += 1;
+                    }
+                    
+                }
+			}
+		}
+	}
+}
