2a3aefec9e28581bdb0632699d2638ceef9532ef
jeremy bedolla <jjbedolla@gmail.com>
2020-11-24 18:00:27 -0500

    inti

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,23 @@
+def rain(walls):
+
+    result = 0
+    left_max = 0
+    right_max = 0
+    lo = 0
+    hi = len(walls) - 1
+
+    while(lo <= hi):
+        if(walls[lo] < walls[hi]):
+            if(walls[lo] > left_max):
+                left_max = walls[lo]
+            else:
+                result += left_max - walls[lo]
+            lo += 1
+        else:
+            if(walls[hi] > right_max):
+                right_max = walls[hi]
+            else:
+                result += right_max - walls[hi]
+            hi -= 1
+
+    return result
