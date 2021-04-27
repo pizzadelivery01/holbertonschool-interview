a53aa9cc58c37ae0626105f3257936edd7523243
jeremy bedolla <jjbedolla@gmail.com>
2021-04-27 18:56:21 -0400

    push

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Island Perimeter Algo
+"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
